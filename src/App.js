5021d43b6d3ad84c74d6e9fff6b5cf9278276c97
Reo Matsuda <reo.matsuda@gmail.com>
2022-01-29 14:41:38 -0600

    Revert "packages for Finnhub"
    This reverts commit 4a37f413aa720da6b1ed061c9c3420635325b43b.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import logo from './logo.svg';
-import React from 'react';
 import './App.css';
 
 function App() {
-  const finnhub = require('finnhub');
-
-  const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-  api_key.apiKey = "c7qobl2ad3ia6nr4s5o0"
-  const finnhubClient = new finnhub.DefaultApi()
-
-  finnhubClient.quote("AAPL", (error, data, response) => {
-    console.log(data)
-  });
-
   return (
     <div className="App">
       <header className="App-header">
@@ -26,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-         hello
+          Learn React
         </a>
       </header>
     </div>

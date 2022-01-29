56d63f8dc82d74f6e8cd4c99debfb21352b8d6c3
Reo Matsuda <reo.matsuda@gmail.com>
2022-01-29 14:46:04 -0600

    Fixed FinnHub

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const finnhub = require('finnhub');
+
+  const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+  api_key.apiKey = "c7qobl2ad3ia6nr4s5o0"
+  const finnhubClient = new finnhub.DefaultApi()
+
+  finnhubClient.quote("AAPL", (error, data, response) => {
+    console.log(data)
+  });
+  
   return (
     <div className="App">
       <header className="App-header">
